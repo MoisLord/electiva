814751a9c39db582d6efcac6c6743429e2475b1d
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-11-05 22:32:20 -0400

    Proveedores

--- Grupo 02/js/menu.js
+++ Grupo 02/js/menu.js
@@ -6,15 +6,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const categoriasLink = document.getElementById("categorias");
     const proveedoresLink = document.getElementById("proveedores");
 
-    // Agregar evento de clic para "MODELO DE PRODUCTOS"
-    modeloLink.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
-        window.location.href = "modelo.html"; // Redirigir a modelo.html
-    });
-
     // Agregar evento de clic para "CATEGORIAS DE PRODUCTOS"
     categoriasLink.addEventListener("click", function(event) {
         event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
         window.location.href = "categorias.html"; // Redirigir a categorias.html
+    });
+
+    // Agregar evento de clic para "PROVEEDORES"
+    proveedoresLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+        window.location.href = "proveedores.html"; // Redirigir a modelo.html
     });
 });
