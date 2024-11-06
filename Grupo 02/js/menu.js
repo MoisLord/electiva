c75b2903944a29b972690de02662591632c508c0
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-11-05 22:39:54 -0400

    menu.js

--- Grupo 02/js/menu.js
+++ Grupo 02/js/menu.js
@@ -2,19 +2,19 @@ window.location.reload()
 
 // Esperar a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function() {
-    // Seleccionar los elementos por su ID
+    // Obtener los elementos de las categorías y proveedores
     const categoriasLink = document.getElementById("categorias");
     const proveedoresLink = document.getElementById("proveedores");
 
-    // Agregar evento de clic para "CATEGORIAS DE PRODUCTOS"
+    // Agregar evento de clic para redirigir a categorias.html
     categoriasLink.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+        event.preventDefault(); // Evitar el comportamiento por defecto del enlace
         window.location.href = "categorias.html"; // Redirigir a categorias.html
     });
 
-    // Agregar evento de clic para "PROVEEDORES"
+    // Agregar evento de clic para redirigir a proveedores.html
     proveedoresLink.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+        event.preventDefault(); // Evitar el comportamiento por defecto del enlace
         window.location.href = "proveedores.html"; // Redirigir a proveedores.html
     });
 });
